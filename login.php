85ae737a05266f7d199fdfd6c97ca5610db45e4a
24160804-pixel <24160804@itoaxaca.edu.mx>
2026-06-03 08:58:03 -0600

    Puse en mayusuculas panel de gestion en el login.php

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body class="bg-light">
     <div class="login-container">
         <div class="card login-card">
-            <h2 class="text-blue">Panel de Gestión</h2>
+            <h2 class="text-blue">PANEL DE GESTIÓN</h2>
             <form action="login.php" method="POST">
                 <input type="email" name="correo" placeholder="tunumcontrol@itoaxaca.edu.mx" required>
                 <input type="password" name="password" placeholder="Contraseña" required>
